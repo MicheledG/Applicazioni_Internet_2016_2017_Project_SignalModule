fcfc21151fa74d7db172015d96fe203345b66d51
MicheledG <chiefzephyr@gmail.com>
2017-06-10 10:15:32 +0200

    initial commit

---
+++ src/main/java/it/polito/ai/signalmodule/SignalModuleApplication.java
@@ -0,0 +1,12 @@
+package it.polito.ai.signalmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalModuleApplication.class, args);
+	}
+}
