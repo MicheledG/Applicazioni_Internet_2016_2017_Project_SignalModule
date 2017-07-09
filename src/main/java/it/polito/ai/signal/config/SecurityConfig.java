03b1574710b1da55f618acd12fb4d5c40a9d65d1
Francesco Longo <france193.htc@gmail.com>
2017-07-09 19:20:38 +0200

    line formatting

--- src/main/java/it/polito/ai/signal/config/SecurityConfig.java
+++ src/main/java/it/polito/ai/signal/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package it.polito.ai.signal.config;
 
+import it.polito.ai.signal.security.JWTAuthenticationEntryPoint;
+import it.polito.ai.signal.security.JWTAuthenticationFilter;
+import it.polito.ai.signal.security.JWTAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
@@ -13,52 +16,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import it.polito.ai.signal.security.JWTAuthenticationEntryPoint;
-import it.polito.ai.signal.security.JWTAuthenticationFilter;
-import it.polito.ai.signal.security.JWTAuthenticationProvider;
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Autowired
+
+    @Autowired
     private JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-	
-	@Autowired
-	private JWTAuthenticationFilter jwtAuthenticationFilter;
-	
-	@Autowired
+
+    @Autowired
+    private JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
     private JWTAuthenticationProvider jwtAuthenticationProvider;
-	
-	@Override
-    public void configure(AuthenticationManagerBuilder auth)  throws Exception {
-		// Define a custom authentication provider
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // Define a custom authentication provider
         auth.authenticationProvider(jwtAuthenticationProvider);
     }
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			// Disable CSRF protection since tokens are immune to it
-			.csrf().disable()
-			// If the user is not authenticated, returns 401
-			.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
-			// This is a stateless application, disable sessions
-			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			// Security policy
-			.authorizeRequests()
-				// Excepted for CORS preflighted requests
-				.antMatchers(HttpMethod.OPTIONS).permitAll()
-				// Allow only requests on the web socket api
-				.antMatchers("/signal-websocket/**").permitAll()
-				// Any request must be authenticated
-				.anyRequest().authenticated().and()
-			// Custom filter for authenticating users using tokens
-			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-			// Disable resource caching, enable only if the client app is external to this modoule
-			.headers().cacheControl();
-	}	
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                // Disable CSRF protection since tokens are immune to it
+                .csrf().disable()
+                // If the user is not authenticated, returns 401
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
+                // This is a stateless application, disable sessions
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                // Security policy
+                .authorizeRequests()
+                // Excepted for CORS preflighted requests
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                // Allow only requests on the web socket api
+                .antMatchers("/signal-websocket/**").permitAll()
+                // Any request must be authenticated
+                .anyRequest().authenticated().and()
+                // Custom filter for authenticating users using tokens
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                // Disable resource caching, enable only if the client app is external to this modoule
+                .headers().cacheControl();
+    }
 
 }
